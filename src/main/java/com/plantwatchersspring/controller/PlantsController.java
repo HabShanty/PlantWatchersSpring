eaf43647f9e5a56cac9443d41a02a79fa825799b
HabShanty <90073936+HabShanty@users.noreply.github.com>
2023-04-18 08:43:32 -0400

    Fixed plant repo

--- src/main/java/com/plantwatchersspring/controller/PlantsController.java
+++ src/main/java/com/plantwatchersspring/controller/PlantsController.java
@@ -1,4 +1,47 @@
 package com.plantwatchersspring.controller;
 
+import com.plantwatchersspring.entity.Plant;
+import com.plantwatchersspring.entity.Users;
+import com.plantwatchersspring.repository.PlantRepository;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class PlantsController {
+
+    @Autowired
+    PlantRepository plantRepo;
+
+    @PostMapping("/plant")
+    public ResponseEntity<Plant> save(@RequestBody Plant plant) {
+        try {
+            return new ResponseEntity<>(plantRepo.save(plant), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/plant")
+    public ResponseEntity<List<Plant>> getAllUsers(){
+        try{
+            List<Plant> list = plantRepo.findAll();
+            if (list.isEmpty() || list.size() == 0){
+                return new ResponseEntity<List<Plant>>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<List<Plant>>(list, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+
 }
--- src/main/java/com/plantwatchersspring/repository/PlantRepository.java
+++ src/main/java/com/plantwatchersspring/repository/PlantRepository.java
@@ -3,14 +3,16 @@ package com.plantwatchersspring.repository;
 import com.plantwatchersspring.entity.Plant;
 import java.util.List;
 
-import com.plantwatchersspring.entity.Users;
+import com.plantwatchersspring.entity.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlantRepository extends
-        PagingAndSortingRepository<Users, Long>,
-        JpaRepository<Users, Long> {
+        PagingAndSortingRepository<Plant, Long>,
+        JpaRepository<Plant, Long> {
     /*
     List<Plant> findPlantByUserId(Long id);
 
